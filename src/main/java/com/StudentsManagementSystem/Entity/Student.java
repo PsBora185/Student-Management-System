39b9dd8114bf6d6b55880fd3317e6e96944f22a5
pranav <pranavsinghbora@gmail.com>
2025-09-06 20:49:32 +0530

    Student Object Class

---
+++ src/main/java/com/StudentsManagementSystem/Entity/Student.java
@@ -0,0 +1,64 @@
+package com.StudentsManagementSystem.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "First_Name")
+    private String firstName;
+
+    @Column(name = "Last_Name")
+    private String lastName;
+
+    @Column(name = "Email")
+    private String email;
+
+    public Student(int id, String firstName, String lastName, String email) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Student() {
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
