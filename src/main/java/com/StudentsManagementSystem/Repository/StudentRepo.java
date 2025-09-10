228fcd6076c0fcf7bed8403bf68f3149c4f0b539
pranav <pranavsinghbora@gmail.com>
2025-09-10 21:50:12 +0530

    Updated repository , added searching for student

--- src/main/java/com/StudentsManagementSystem/Repository/StudentRepo.java
+++ src/main/java/com/StudentsManagementSystem/Repository/StudentRepo.java
@@ -2,7 +2,10 @@ package com.StudentsManagementSystem.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.StudentsManagementSystem.Entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface StudentRepo extends JpaRepository<Student, Integer> {  // provides methods for data
-
+    Page<Student> findByFirstNameOrLastNameOrEmail(
+            String firstName, String lastName, String email, Pageable pageable);
 }
