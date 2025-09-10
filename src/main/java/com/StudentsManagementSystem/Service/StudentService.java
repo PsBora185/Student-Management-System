7ac07b3eed510d608f988efa6d406cf7d1164fe1
pranav <pranavsinghbora@gmail.com>
2025-09-10 21:51:07 +0530

    Added more methods

--- src/main/java/com/StudentsManagementSystem/Service/StudentService.java
+++ src/main/java/com/StudentsManagementSystem/Service/StudentService.java
@@ -2,6 +2,8 @@ package com.StudentsManagementSystem.Service;
 
 import java.util.List;
 import com.StudentsManagementSystem.Entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface StudentService {
 
@@ -12,4 +14,12 @@ public interface StudentService {
     public Student getById(int id);
 
     public void deleteById(int id);
+
+    Page<Student> getAllStudents(Pageable pageable);
+
+    Page<Student> searchStudents(String keyword, Pageable pageable);
+
+    long countAllStudents();
+
+
 }
